921088c788d004c67a5f07354886bab1d650522d
facupascale <facupascale@gmail.com>
2024-04-19 13:54:54 +0200

    nueva build

---
+++ expo-client-updates/App.js
@@ -0,0 +1,42 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import * as Updates from "expo-updates";
+import { useEffect } from "react";
+
+export default function App() {
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      // You can also add an alert() to see the error message in case of an error when fetching updates.
+      alert(`Error fetching latest Expo update: ${error}`);
+    }
+  }
+
+  useEffect(() => {
+    onFetchUpdateAsync();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>esto es un quinto cambio</Text>
+      <Text>{Updates.isAvailable ? "true" : "false"}</Text>
+      <Image source={require("./assets/favicon.png")} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ expo-client-updates/app.config.js
@@ -0,0 +1,120 @@
+const commonConfig = {
+  expo: {
+    plugins: ["expo-build-properties"],
+    extra: {
+      eas: {
+        projectId: "e8a8a300-83dc-4b8f-b701-3ae6366751ed",
+      },
+    },
+    slug: "com.test.updates",
+    ios: {
+      bundleIdentifier: "com.test.updates",
+      infoPlist: {
+        NSAppTransportSecurity: {
+          NSAllowsArbitraryLoads: true,
+          NSExceptionDomains: {
+            "http://66.97.34.155": {
+              NSIncludesSubdomains: true,
+              NSThirdPartyExceptionRequiresForwardSecrecy: false,
+            },
+          },
+        },
+        expoPlist: {
+          EXUpdatesUrl: "http://66.97.34.155:3000/api/manifest",
+          EXUpdatesCodeSigningMetadata: {
+            keyid: "main",
+            alg: "rsa-v1_5-sha256",
+          },
+          EXUpdatesCheckOnLaunch: "ALWAYS",
+          EXUpdatesEnabled: true,
+          EXUpdatesLaunchWaitMs: 30000,
+          EXUpdatesRuntimeVersion: 1,
+          EXUpdatesCodeSigningCertificate:
+            "-----BEGIN CERTIFICATE-----\nMIICzTCCAbWgAwIBAgIJGnHf0osQEoc2MA0GCSqGSIb3DQEBCwUAMA8xDTALBgNV\nBAMTBHRlc3QwIBcNMjIwNDE0MTY1MjE3WhgPMjEyMjA0MTQxNjUyMTdaMA8xDTAL\nBgNVBAMTBHRlc3QwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDS+Jol\nAnxgRGlsfS5zTajApxCH2+Zg8q/b/CMDNprvkT19eRL/CL41O84v4BQICJ1nQOFs\n+DwmMwoi0iLeoT3tz2wTeS/pC0lKusx8KiSZrVh3HImYmEahSF5qznNEUNyXO74q\nKBf+1wINNKK4igcqrKyrfCDus76fOZ374WUMPMZn9pxFv5kqovpgCQBhdJ9IqQTQ\nZpV1qoyHwMmA3fDDsNTdGc127lbVli96pTlOmIuoX7sUdYJlTVmMr5KIvZmh95Wz\nPOihh5O1V7TGy7Ybfm9+tSHkqosO2dg6YTEZOxMxAM+6rERTtdOkuO0tlmRdiqP5\n/2ze3k4dzDMcCTn/AgMBAAGjKjAoMA4GA1UdDwEB/wQEAwIHgDAWBgNVHSUBAf8E\nDDAKBggrBgEFBQcDAzANBgkqhkiG9w0BAQsFAAOCAQEAUjjv9YiWBtbFfheB+QTK\nepnwCOH87e+ZZeFgQ4zOa+XKISKQ5EPGqDXXusSn+vf6n0Xz9TIZa+f0afPfefZi\n6k3KbSxmZ9XiZGPdI2hTbo8VUCxuYvdSB8R/Y5Gpabt1Wpuub//HHcORfr04WPCz\nAyIseRunLzxcT9X9z55XdhsoTo4V8AnkihFgjB8pNMKb6yfSM8YtvXfNR4QCusoK\nbbzsc2bURI2v2z7G6w76sQBdKGswG2gC2HGxhlAXMSJvJZyvQMXP+RaiLRMvDxhr\nNL8e5Y4+/HH1/m1EiXW3jssglgwGTdXG+3yS3z/CR2+ZpH9DuGR3II8T5AL6cGOi\nZA==\n-----END CERTIFICATE-----",
+        },
+      },
+    },
+    android: {
+      package: "com.test.updates",
+      manifest: {
+        application: {
+          $: {
+            "android:name": "usesCleartextTraffic",
+            "android:value": "true",
+          },
+          "meta-data": [
+            {
+              $: {
+                "android:name": "expo.modules.updates.CODE_SIGNING_CERTIFICATE",
+                "android:value":
+                  "-----BEGIN CERTIFICATE-----\nMIICzTCCAbWgAwIBAgIJGnHf0osQEoc2MA0GCSqGSIb3DQEBCwUAMA8xDTALBgNV\nBAMTBHRlc3QwIBcNMjIwNDE0MTY1MjE3WhgPMjEyMjA0MTQxNjUyMTdaMA8xDTAL\nBgNVBAMTBHRlc3QwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDS+Jol\nAnxgRGlsfS5zTajApxCH2+Zg8q/b/CMDNprvkT19eRL/CL41O84v4BQICJ1nQOFs\n+DwmMwoi0iLeoT3tz2wTeS/pC0lKusx8KiSZrVh3HImYmEahSF5qznNEUNyXO74q\nKBf+1wINNKK4igcqrKyrfCDus76fOZ374WUMPMZn9pxFv5kqovpgCQBhdJ9IqQTQ\nZpV1qoyHwMmA3fDDsNTdGc127lbVli96pTlOmIuoX7sUdYJlTVmMr5KIvZmh95Wz\nPOihh5O1V7TGy7Ybfm9+tSHkqosO2dg6YTEZOxMxAM+6rERTtdOkuO0tlmRdiqP5\n/2ze3k4dzDMcCTn/AgMBAAGjKjAoMA4GA1UdDwEB/wQEAwIHgDAWBgNVHSUBAf8E\nDDAKBggrBgEFBQcDAzANBgkqhkiG9w0BAQsFAAOCAQEAUjjv9YiWBtbFfheB+QTK\nepnwCOH87e+ZZeFgQ4zOa+XKISKQ5EPGqDXXusSn+vf6n0Xz9TIZa+f0afPfefZi\n6k3KbSxmZ9XiZGPdI2hTbo8VUCxuYvdSB8R/Y5Gpabt1Wpuub//HHcORfr04WPCz\nAyIseRunLzxcT9X9z55XdhsoTo4V8AnkihFgjB8pNMKb6yfSM8YtvXfNR4QCusoK\nbbzsc2bURI2v2z7G6w76sQBdKGswG2gC2HGxhlAXMSJvJZyvQMXP+RaiLRMvDxhr\nNL8e5Y4+/HH1/m1EiXW3jssglgwGTdXG+3yS3z/CR2+ZpH9DuGR3II8T5AL6cGOi\nZA==\n-----END CERTIFICATE-----\n",
+              },
+            },
+            {
+              $: {
+                "android:name": "expo.modules.updates.ENABLED",
+                "android:value": "true",
+              },
+            },
+            {
+              $: {
+                "android:name": "expo.modules.updates.EXPO_UPDATE_URL",
+                "android:value": "http://66.97.34.155:3000/api/manifest",
+              },
+            },
+            {
+              $: {
+                "android:name": "expo.modules.updates.EXPO_RUNTIME_VERSION",
+                "android:value": "1",
+              },
+            },
+            {
+              $: {
+                "android:name":
+                  "expo.modules.updates.EXPO_UPDATES_CHECK_ON_LAUNCH",
+                "android:value": "ALWAYS",
+              },
+            },
+            {
+              $: {
+                "android:name":
+                  "expo.modules.updates.EXPO_UPDATES_LAUNCH_WAIT_MS",
+                "android:value": "30000",
+              },
+            },
+          ],
+        },
+      },
+    },
+    privacy: "unlisted",
+    androidStatusBar: {
+      barStyle: "light-content",
+    },
+    icon: "./assets/icon.png",
+    platforms: ["ios", "android"],
+    orientation: "portrait",
+    updates: {
+      enabled: true,
+      fallbackToCacheTimeout: 0,
+      checkAutomatically: "ON_ERROR_RECOVERY",
+      url: "http://66.97.34.155:3000/api/manifest",
+      codeSigningCertificate: "./code-signing/certificate.pem",
+      codeSigningMetadata: {
+        keyid: "main",
+        alg: "rsa-v1_5-sha256",
+      },
+    },
+    assetBundlePatterns: ["**/*"],
+    splash: {
+      image: "./assets/splash.png",
+      backgroundColor: "#ffffff",
+    },
+  },
+  // jsEngine: "hermes",
+};
+
+module.exports = () => {
+  return {
+    ...commonConfig,
+  };
+};
